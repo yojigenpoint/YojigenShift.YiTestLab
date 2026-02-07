47aff6f28606d7bbabe203cdd4ef367212d45405
Yojigen Point <yojigenpoint@proton.me>
2026-02-07 11:34:39 -0500

    feat: Add initial project structure with configuration files, localization, and main layout

---
+++ yi-test-lab/Scripts/UI/MainLayout.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System.Globalization;
+
+public partial class MainLayout : Node
+{
+	private TabContainer _contentTabs;
+	private OptionButton _langSelector;
+	private Button _btnWuxing;
+	private Button _btnGanzhi;
+
+	public override void _Ready()
+	{
+		_contentTabs = GetNode<TabContainer>("AppSplit/ContentTabs");
+
+		_langSelector = GetNode<OptionButton>("AppSplit/Sidebar/LanguageSelector");
+
+		_btnWuxing = GetNode<Button>("AppSplit/Sidebar/Btn_WuXing");
+		_btnGanzhi = GetNode<Button>("AppSplit/Sidebar/Btn_GanZhi");
+
+		_langSelector.ItemSelected += OnLanugageChanged;
+
+		_btnWuxing.Pressed += () => SwitchTab(0);
+		_btnGanzhi.Pressed += () => SwitchTab(1);
+
+		TranslationServer.SetLocale("zh_CN");
+		_langSelector.Selected = 0;
+	}
+
+	private void OnLanugageChanged(long index)
+	{
+		string langCode = index == 0 ? "zh_CN" : "en_US";
+
+		TranslationServer.SetLocale(langCode);
+
+		var culture = new CultureInfo(langCode == "zh" ? "zh-CN" : "en-US");
+		CultureInfo.CurrentCulture = culture;
+		CultureInfo.CurrentUICulture = culture;
+
+		//ReloadCurrentTab();
+	}
+
+	private void SwitchTab(int index)
+	{
+		_contentTabs.CurrentTab = index;
+	}
+}
